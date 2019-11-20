dec780524a0f41b78d119d3f2a4c1f6db9155ed4
leon <applepip@163.com>
2019-11-20 17:54:52 +0800

    可视化tensorflow节点

---
+++ tf_linear_regression/tf_tensorboard.py
@@ -0,0 +1,72 @@
+# 进入 TensorBoard。根据所提供一些训练统计信息，它将在您的网络浏览器中
+# 显示这些统计信息的良好交互式可视化（例如学习曲线）。
+
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+import tensorflow as tf
+
+housing = fetch_california_housing()
+m, n = housing.data.shape
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaled_housing_data = scaler.fit_transform(housing.data)
+scaled_housing_data_plus_bias = np.c_[np.ones((m, 1)), scaled_housing_data]
+
+print(scaled_housing_data_plus_bias.mean(axis=0))
+print(scaled_housing_data_plus_bias.mean(axis=1))
+print(scaled_housing_data_plus_bias.mean())
+print(scaled_housing_data_plus_bias.shape)
+
+from datetime import datetime
+now = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+root_logdir = "tf_logs"
+logdir = "{}/run-{}/".format(root_logdir, now)
+
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.placeholder(tf.float32, shape=(None, n + 1), name="X")
+y = tf.placeholder(tf.float32, shape=(None, 1), name="y")
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+
+mse_summary = tf.summary.scalar('MSE', mse)
+
+file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
+
+n_epochs = 10
+batch_size = 100
+n_batches = int(np.ceil(m / batch_size))
+
+def fetch_batch(epoch, batch_index, batch_size):
+    np.random.seed(epoch * n_batches + batch_index)  # not shown in the book
+    indices = np.random.randint(m, size=batch_size)
+    X_batch = scaled_housing_data_plus_bias[indices]
+    y_batch = housing.target.reshape(-1, 1)[indices]
+    return X_batch, y_batch
+
+with tf.Session() as sess:                                                        # not shown in the book
+    sess.run(init)
+
+    for epoch in range(n_epochs):                                                 # not shown
+        for batch_index in range(n_batches):
+            X_batch, y_batch = fetch_batch(epoch, batch_index, batch_size)
+            if batch_index % 10 == 0:
+                summary_str = mse_summary.eval(feed_dict={X: X_batch, y: y_batch})
+                step = epoch * n_batches + batch_index
+                file_writer.add_summary(summary_str, step)
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+
+
+    best_theta = theta.eval()
+
+    print(best_theta)
+
+file_writer.close()
